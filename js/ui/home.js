4af7b3c8a7b52ee9747e3add2538b8b219cb94d3
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-12-03 18:39:19 +0530

    Show an error message if no CSV files are found
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/ui/home.js
+++ js/ui/home.js
@@ -7,6 +7,15 @@
             $("#home section").innerHTML = riot.render($("#tmpl-home-stock").innerHTML.trim());
         });
 
+        /**
+         * Show noresults if no results are found
+         */
+        api.on("noResults", function () {
+            $("#home section").innerHTML = riot.render($("#tmpl-error").innerHTML.trim(), {
+                message: "No CSV files found for importing."
+            });
+        });
+
     });
 
 })();
